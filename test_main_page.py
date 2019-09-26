c40b9922b2c244b173f963fc60ef6fdb4fe3f169
Johnnyvintage <xdamngood@gmail.com>
2019-09-27 01:46:04 +0300

    Add jump to login method and check

--- test_main_page.py
+++ test_main_page.py
@@ -19,5 +19,5 @@ def test_guest_should_see_authorization_forms(browser):
     page = MainPage(browser, LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()
-    page = LoginPage(browser, browser.current_url)
-    page.should_be_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
