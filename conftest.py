697c6d4f8f5bf0b39726c820ae1500bcb87e5c5c
Johnnyvintage <xdamngood@gmail.com>
2019-09-27 01:34:51 +0300

    Add login form check, change waiting time, move locators to locators.py

--- conftest.py
+++ conftest.py
@@ -29,6 +29,6 @@ def browser(request):
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    time.sleep(2)
+    #time.sleep(2)
     print("\nquit browser..")
     browser.quit()
