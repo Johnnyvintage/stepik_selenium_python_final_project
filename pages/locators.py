b672b19d8c93b824a76c69275f58d59927816899
Johnnyvintage <xdamngood@gmail.com>
2019-09-28 19:00:42 +0300

    Add product page tests
    Added new tests for product page, appropriate locators,
    solve quiz method (with switching from alert back to main window),
    added parametrization with 10 links for product tests,
    added geckodriver.log to .gitignore

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,15 @@ class LoginPageLocators():
     LOGIN_LINK = "/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    PRODUCT_NAME = (By.XPATH, '//div[contains(@class,"product_main")]/h1')
+    BASKET_SUM_MESSAGE = (By.XPATH, '//div[contains(@class,"alert-info")]//strong')
+    SUCCESS_MESSAGE = (By.XPATH, '//div[contains(@class,"alert-success")]//strong')
+    # LET'S TRY CSS SELECTORS, FOLKS!
+    # PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    # BASKET_SUM_MESSAGE = (By.CSS_SELECTOR, ".alert-info > .alertinner > p > strong")
+    # SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success > div > strong')
---
+++ pages/product_page.py
@@ -0,0 +1,44 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def find_str_in_list(value, strlist):
+    if type(strlist) == list:
+        for i in strlist:
+            if value == i:
+                return True
+    return False
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.click_button()
+        self.solve_quiz_and_get_code()
+        self.check_basket_sum()
+        self.check_success_add_message()
+
+    def click_button(self):
+        button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        button.click()
+
+    def check_basket_sum(self):
+        price = (self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)).text
+        basket_message = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located(ProductPageLocators.BASKET_SUM_MESSAGE))
+        print("basket_message", basket_message)
+        basket_sum = basket_message.text
+        print("basket_sum", basket_sum)
+        assert price == basket_sum, f"basket sum {basket_sum} and price of the product {price} don't match!"
+
+    def check_success_add_message(self):
+        success_messages = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_all_elements_located(ProductPageLocators.SUCCESS_MESSAGE))
+        success_texts = [i.text for i in success_messages]
+        name_of_product = (self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)).text
+        print("name_of_product:", name_of_product, "\nsuccess_texts:", success_texts)
+        assert find_str_in_list(name_of_product, success_texts), \
+            f"there is no {name_of_product} in adding to cart messages!"
+
+
