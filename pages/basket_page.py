d7ca270c81960393193860d7bb84da91f150acc9
Johnnyvintage <xdamngood@gmail.com>
2019-10-01 23:26:26 +0300

    Change in basket checks according to teacher demands

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -8,9 +8,21 @@ class BasketPage(BasePage):
         assert BasketPageLocators.BASKET_LINK in url, f"Basket link is incorrect: {url}"
 
     def should_be_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_MESSAGE) \
-               and self.is_not_element_present(*BasketPageLocators.BASKET_ITEM), "Basket is not empty"
+        self.should_be_empty_basket_message()
+        self.shouldnt_be_filled_basket()
+
+    def should_be_empty_basket_message(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_MESSAGE), "There is no message that basket is empty"
+
+    def shouldnt_be_filled_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEM), "There is items in basket but shouldn't"
 
     def should_be_products_in_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.EMPTY_MESSAGE) \
-               or self.is_element_present(*BasketPageLocators.BASKET_ITEM), "There is no products in basket"
+        self.shouldnt_be_empty_basket_message()
+        self.should_be_filled_basket()
+
+    def shouldnt_be_empty_basket_message(self):
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_MESSAGE), "There is empty message in basket"
+
+    def should_be_filled_basket(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_ITEM), "There are no items in basket, but should be"
